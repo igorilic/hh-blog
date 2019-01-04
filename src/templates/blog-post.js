3dec04c22cbdbfbdbe9461828cbd1e5b316bf74e
Igor Ilić <igor.ilic@evia.de>
2019-01-04 11:22:16 +0100

    init

---
+++ src/templates/blog-post.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+import MDXRenderer from 'gatsby-mdx/mdx-renderer'
+
+import Bio from '../components/Bio'
+import Layout from '../components/Layout'
+import SEO from '../components/seo'
+import { rhythm, scale } from '../utils/typography'
+import { gray } from 'ansi-colors';
+
+class BlogPostTemplate extends React.Component {
+  render() {
+    const post = this.props.data.mdx
+    const siteTitle = this.props.data.site.siteMetadata.title
+    const { previous, next } = this.props.pageContext
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title={post.frontmatter.title} description={post.excerpt} />
+        <h1 style={{
+          marginTop: rhythm(3),
+        }}>{post.frontmatter.title}</h1>
+        <p
+          style={{
+            ...scale(-1 / 5),
+            display: `block`,
+            marginBottom: rhythm(1 / 4),
+            marginTop: rhythm(1),
+            color: gray(41),
+            fontStyle: 'italic',
+          }}
+        >
+          {post.frontmatter.date}
+        </p>
+        <MDXRenderer>{post.code.body}</MDXRenderer>
+        <hr
+          style={{
+            marginBottom: rhythm(1),
+          }}
+        />
+        <Bio />
+
+        <ul
+          style={{
+            display: `flex`,
+            flexWrap: `wrap`,
+            justifyContent: `space-between`,
+            listStyle: `none`,
+            padding: 0,
+          }}
+        >
+          <li>
+            {previous && (
+              <Link to={previous.fields.slug} rel="prev">
+                ← {previous.frontmatter.title}
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Link to={next.fields.slug} rel="next">
+                {next.frontmatter.title} →
+              </Link>
+            )}
+          </li>
+        </ul>
+      </Layout>
+    )
+  }
+}
+
+export default BlogPostTemplate
+
+export const pageQuery = graphql`
+  query($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+    mdx(fields: { slug: { eq: $slug } }) {
+      id
+      excerpt(pruneLength: 160)
+      frontmatter {
+        title
+        date(formatString: "D. M. 'YY.")
+      }
+      code {
+        body
+      }
+    }
+  }
+`
---
+++ src/utils/typography.js
@@ -0,0 +1,23 @@
+import Typography from 'typography'
+import twinPeaksTheme from 'typography-theme-twin-peaks'
+
+twinPeaksTheme.overrideThemeStyles = () => {
+  return {
+    'a.gatsby-resp-image-link': {
+      boxShadow: `none`,
+    },
+  }
+}
+
+// delete Wordpress2016.googleFonts
+
+const typography = new Typography(twinPeaksTheme)
+
+// Hot reload typography in development.
+if (process.env.NODE_ENV !== `production`) {
+  typography.injectStyles()
+}
+
+export default typography
+export const rhythm = typography.rhythm
+export const scale = typography.scale
